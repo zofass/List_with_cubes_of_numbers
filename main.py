9572b9962f0f4ef08f28e1360dcaf90f2d07010b
zofass <chachachka@gmail.com>
2023-12-08 11:32:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from inspect import isgenerator
+
+def generate_cube_numbers(end):
+    num = 2
+    while num**3 <= end:
+        yield num**3
+        num += 1
+
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print('OK')
